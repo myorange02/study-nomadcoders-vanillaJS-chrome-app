fac12082f2b419d661007413d1d059dc4b0d5613
myorange02 <94654972+myorange02@users.noreply.github.com>
2024-10-06 13:48:42 +0000

    #7.2 Deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  const button = document.createElement("button");
   span.innerText = newToDo;
+  
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  
   li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
