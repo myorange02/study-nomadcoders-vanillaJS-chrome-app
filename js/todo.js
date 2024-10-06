a40657e19c48603e9bc0f7962212040c73d54085
myorange02 <94654972+myorange02@users.noreply.github.com>
2024-10-06 13:31:29 +0000

    #7.1 demonstration

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newToDo;
+  li.appendChild(span);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
