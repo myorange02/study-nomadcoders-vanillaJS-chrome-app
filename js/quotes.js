e9f056cf61b3ad24100a0ca9870997d521c88f81
myorange02 <94654972+myorange02@users.noreply.github.com>
2024-10-20 13:42:51 +0000

    changing backgrounds and adding shadows for inputs and buttons

--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,43 @@
 const quotes = [
 {
-quote: 'I never dreamed about success, I worked for it',
-author: 'Estee Lauder'
+quote: "I can't do this all day.",
+author: 'Steve Rodgers'
 },
 {
-quote: 'Do not try to be original, just try to be good.',
-author: 'Paul Rand'
+quote: 'Just Do It Later',
+author: 'Nike'
 },
 {
-quote: 'Do not be afraid to give up the good to go for the great',
-author: 'John D. Rockefeller'
+quote: 'That was not easy.',
+author: 'Staples'
 },
 {
-quote: 'If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.',
-author: 'Martin Luther King Jr.'
+quote: "I'm gonna be the king of the pirates!",
+author: 'Monkey D Luffy (One Piece)'
 },
 {
-quote: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
-author: 'Thomas Edison'
+quote: 'Why is it working?',
+author: 'One of the Jr. Developers'
 },
 {
-quote: 'The fastest way to change yourself is to hang out with people who are already the way you want to be',
-author: 'REid Hoffman'
+quote: 'print("give me some sleep")',
+author: 'Python'
 },
 {
-quote: 'Money is like gasoline during a road trip. You do not want to run out of gas on your trip, but you are not doing a tour of gas stations',
-author: 'Tim O Reilly'
+quote: 'I love kimchi so much :D',
+author: 'Nicolas Serrano Arevalo'
 },
 {
-quote: 'Some people dream of success, while other people get up every morning and make it happen',
-author: 'Wayne Huizenga'
+quote: 'You become what you believe.',
+author: 'Pawn (Chess)'
 },
 {
-quote: 'The only thing worse than starting something and falling.. is not starting something',
-author: 'SEth Godin'
+quote: 'A red horned one is three times as fast!',
+author: 'Mobile Suit Gundam'
 },
 {
-quote: 'If you really want to do something, you will find a way. If you do not, you will find an excuse.',
-author: 'Jim Rohn'
+quote: 'All the world on one arrow.',
+author: 'Ashe (League of Legends)'
 },
 ];
 
